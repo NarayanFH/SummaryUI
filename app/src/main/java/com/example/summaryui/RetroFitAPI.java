ce2f253c551ef74d5705c7d1ad57f441207577fe
NarayanFintoo <narayan.gupta@fintoo.in>
2022-07-08 17:20:53 +0530

    post api

--- app/src/main/java/com/example/summaryui/RetroFitAPI.java
+++ app/src/main/java/com/example/summaryui/RetroFitAPI.java
@@ -1,2 +1,16 @@
-package com.example.summaryui;public interface RetroFitAPI {
+package com.example.summaryui;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public interface RetroFitAPI {
+
+    // as we are making a post request to post a data
+    // so we are annotating it with post
+    // and along with that we are passing a parameter as users
+    @POST("users")
+
+    //on below line we are creating a method to post our data.
+    Call<DataModal> createPost(@Body DataModal dataModal);
 }
